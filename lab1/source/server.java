bddddd4feb4df47f70125213ff70e51511108c9e
ninjarlz <michal.kusmidrowicz@gmail.com>
2022-11-05 12:38:55 +0100

    lab1 impl

--- lab1/source/server.java
+++ lab1/source/server.java
@@ -1,12 +1,18 @@
 import java.io.IOException;
 import java.io.OutputStream;
 import java.net.InetSocketAddress;
+import java.time.format.DateTimeFormatter;
+import java.time.ZonedDateTime;
+import java.time.ZoneId;
 
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 import com.sun.net.httpserver.HttpServer;
 
 public class Test {
+
+    private static final String DATE_TIME_PATTERN = "HH:mm:ss";
+    private static final String TIME_ZONE = "UTC+01:00";
 
     public static void main(String[] args) throws Exception {
         HttpServer server = HttpServer.create(new InetSocketAddress(4080), 0);
@@ -18,12 +24,16 @@ public class Test {
     static class MyHandler implements HttpHandler {
         @Override
         public void handle(HttpExchange t) throws IOException {
-            String response = "Hello World from java!\n";
+            StringBuilder responseBuilder = new StringBuilder("Hello World from java!\n");
+            DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern(DATE_TIME_PATTERN);
+            ZonedDateTime zonedDateTime = ZonedDateTime.now(ZoneId.of(TIME_ZONE));
+            responseBuilder.append(dateTimeFormatter.format(zonedDateTime));
+            String response = responseBuilder.toString();
             t.sendResponseHeaders(200, response.length());
             OutputStream os = t.getResponseBody();
             os.write(response.getBytes());
             os.close();
-	    System.out.println("Served hello world...");
+	        System.out.println("Served hello world...");
         }
     }
 
